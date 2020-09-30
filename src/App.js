e84e7cad1d85e4ef688ac59d31354d70d002e3e3
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-09-30 13:29:30 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuMakanan from'./Page/MenuMakanan';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <MenuMakanan/>
+    </div>
+  );
+}
+
+export default App;
